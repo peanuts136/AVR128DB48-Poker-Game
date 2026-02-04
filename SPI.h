baaa98eac46b71d9de6b30695331fab326ff5d4f
Tony He <111782622+peanuts136@users.noreply.github.com>
2026-02-04 02:11:39 -0500

    Update SPI.h

--- SPI.h
+++ SPI.h
@@ -1,8 +1,6 @@
 /*
  * SPI.h
- *
  * Created: 11/19/2025 3:12:55 PM
- * Author : ziyan mei
  */ 
 
 #ifndef SPI_H_
@@ -11,10 +9,11 @@
 #include <avr/io.h>
 #include <stdint.h>
 
-#define NUM_MODULES 4   // Four 8×8 MAX7219 matrices
+#define NUM_MODULES 4   // Four 8Ã—8 MAX7219 matrices
 
 void SPI_Init(void);
 void Matrix_Clear(void);
 void Matrix_DisplayNumber(uint16_t value);
 
 #endif
+
